1ad9900fd809919da013959854e12ca647ef1029
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-17 20:38:30 +0530

    header added

---
+++ src/Components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Constant from "expo-constants";
+export default function Header() {
+  return (
+    <View
+      style={{
+        height: 50,
+        marginTop: Constant.statusBarHeight,
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        elevation: 8,
+      }}
+    >
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginLeft: 20 }}
+      >
+        <Entypo name="youtube" size={28} color="red" />
+        <Text style={{ fontSize: 24, marginLeft: 10, fontWeight: "bold" }}>
+          Youtube
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          width: 150,
+          justifyContent: "space-around",
+        }}
+      >
+        <Ionicons color="#212121" name="md-videocam" size={28} />
+        <Ionicons color="#212121" name="md-search" size={28} />
+        <MaterialIcons color="#212121" name="account-circle" size={28} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
