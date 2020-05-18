a706a147cd8557ec2c34d8e180ca0359df88b57a
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-18 20:28:33 +0530

    Youtube api

--- src/Components/MiniCard.js
+++ src/Components/MiniCard.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
-const MiniCard = () => {
+const MiniCard = ({ id, title, channel, imgurl, channelid }) => {
+  const [channelImg, setchannelImg] = useState("");
+  // const getAvatar = () => {
+  //   fetch(
+  //     `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelid}&fields=items%2Fsnippet%2Fthumbnails&key=AIzaSyAtVkfVRiNbd4RzvSm7-_19uAnjznxpi3g`,
+  //     {
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //         Accept: "application/json",
+  //       },
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setchannelImg(data.items.snippet.thumbnails.default.url));
+  // };
+  // getAvatar();
   return (
     <View style={{ flexDirection: "row", margin: 10, marginBottom: 0 }}>
       <Image
         source={{
-          uri:
-            "https://images.unsplash.com/photo-1534157327728-accacabda257?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+          uri: imgurl,
         }}
         style={{ width: "40%", height: 100 }}
       />
@@ -17,11 +31,13 @@ const MiniCard = () => {
           numberOfLines={2}
           style={{ fontSize: 18, width: 200 }}
         >
-          Title of the video
+          {title}
         </Text>
-        <Text style={{ fontSize: 12, color: "#777" }}>
-          user-458k views-2 hours ago
-        </Text>
+        {/* <Image
+          source={{ uri: channelImg }}
+          style={{ width: 80, height: 80, borderRadius: 40 }}
+        /> */}
+        <Text style={{ fontSize: 12, color: "#777" }}>{channel}</Text>
       </View>
       <Entypo
         name="dots-three-vertical"
--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,10 +1,41 @@
 import React, { useState } from "react";
 
 import { AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { StyleSheet, Text, View, ScrollView, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import MiniCard from "../Components/MiniCard";
+
+//www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=songs&type=video&key=AIzaSyAtVkfVRiNbd4RzvSm7-_19uAnjznxpi3g
 export default function Home() {
   const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [miniCards, setMiniCards] = useState([]);
+  const renderCards = () => {
+    setMiniCards([]);
+    setLoading(true);
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${value}&regionCode=IN&type=video&key=AIzaSyAtVkfVRiNbd4RzvSm7-_19uAnjznxpi3g`
+      // `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${value}&regionCode=India&safeSearch=moderate&type=video&key=AIzaSyAtVkfVRiNbd4RzvSm7-_19uAnjznxpi3g`
+      // {
+      //   headers: {
+      //     "Content-Type": "multipart/form-data",
+      //     Accept: "application/json",
+      //   },
+      // }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setMiniCards(data.items);
+      });
+  };
   return (
     <View style={{ flex: 1 }}>
       <View
@@ -28,24 +59,31 @@ export default function Home() {
           value={value}
           onChangeText={(text) => setValue(text)}
         />
-        <MaterialIcons name="send" size={24} color="black" />
+        <MaterialIcons
+          onPress={renderCards}
+          name="send"
+          size={24}
+          color="black"
+        />
       </View>
-      <ScrollView>
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-        <MiniCard />
-      </ScrollView>
+      {loading ? (
+        <ActivityIndicator style={{ marginTop: 20 }} size="large" color="red" />
+      ) : null}
+      <FlatList
+        data={miniCards}
+        keyExtractor={(item) => item.id.videoId}
+        renderItem={({ item }) => {
+          return (
+            <MiniCard
+              imgurl={item.snippet.thumbnails.medium.url}
+              id={item.id.videoId}
+              title={item.snippet.title}
+              channel={item.snippet.channelTitle}
+              channelid={item.snippet.channelId}
+            />
+          );
+        }}
+      />
     </View>
   );
 }
